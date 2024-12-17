997317a6f8ae952d7111dd8658a95f0b7b8c3783
Matheus Takashi Maruoka Vieira <149627412+TkSiZ@users.noreply.github.com>
2024-12-17 12:49:44 -0300

    Mais estudos

---
+++ React/10_MINIBLOG/miniblog/src/firebase/config.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsyyqytraHR41dDKY1MlHjnsolLXrRVbU",
+  authDomain: "miniblog-bb42f.firebaseapp.com",
+  projectId: "miniblog-bb42f",
+  storageBucket: "miniblog-bb42f.firebasestorage.app",
+  messagingSenderId: "513337800879",
+  appId: "1:513337800879:web:ac158d654f252640127c5a"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
