be8cb76dae8bb0eeca203342ba6f1c1d42d4c52d
Matheus Takashi Maruoka Vieira <matheustakashi2005@gmail.com>
2024-12-24 12:45:28 -0300

    Mais um projeto

---
+++ React/12_REACTGRAM/backend/routes/UserRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router()
+
+// Controller 
+const {register, login} = require("../controllers/UserController")
+
+// Midlawares 
+const validate = require("../middlewares/handleValidation")
+const {userCreateValidation, loginValidation} = require("../middlewares/userValidation")
+
+// Routes
+router.post("/register", userCreateValidation(), validate, register)
+router.post("/login", loginValidation(), validate, login)
+
+module.exports = router
